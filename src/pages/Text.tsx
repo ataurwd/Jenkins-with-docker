76278dcef434207dd010c46f2f21ba03ec25be63
ataurwd <ataurrahman24707@gmail.com>
2026-05-04 11:00:21 +0600

    Test 5

--- src/pages/Text.tsx
+++ src/pages/Text.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 
 const Text = () => {
     return (
         <div>
             [ea_video src="https://easyaccordion.io/wp-content/uploads/2026/05/Easy-Accordion-Getting-Started.mp4"]
+            test case 5
         </div>
     );
 };
